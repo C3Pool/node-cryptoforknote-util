a2f3a91a019eba89687a58d3bd08a4e7d2e8be01
C3Pool <hkfoan@gmail.com>
2020-06-05 15:02:49 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+Node-CryptoForkNote-Util with Merged Mining support
+===================================================
+
+Dependencies
+------------
+
+* Boost (http://www.boost.org/)
---
+++ src/cryptonote_config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define CURRENT_TRANSACTION_VERSION 1
+
+enum BLOB_TYPE {
+  BLOB_TYPE_CRYPTONOTE        = 0,
+  BLOB_TYPE_FORKNOTE1         = 1,
+  BLOB_TYPE_FORKNOTE2         = 2,
+  BLOB_TYPE_CRYPTONOTE2       = 3, // Masari
+  BLOB_TYPE_CRYPTONOTE_RYO    = 4, // Ryo
+  BLOB_TYPE_CRYPTONOTE_LOKI   = 5, // Loki
+  BLOB_TYPE_CRYPTONOTE3       = 6, // Masari
+  BLOB_TYPE_AEON              = 7, // Aeon
+  BLOB_TYPE_CRYPTONOTE_CUCKOO = 8, // MoneroV / Swap
+  BLOB_TYPE_CRYPTONOTE_XTNC   = 9, // XTNC
+};
